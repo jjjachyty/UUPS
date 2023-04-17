86c4c1329e9c1d026a118085560c656c7a468bc4
Janly <jjjachyty@163.com>
2023-04-17 11:17:04 +0800

    1

--- deploy/cto.js
+++ deploy/cto.js
@@ -11,17 +11,13 @@ async function upgraded_test() {
 
   // Upgrading
   const DAPP = await ethers.getContractFactory("CTO");
-  const upgraded = await upgrades.upgradeProxy("0xEd4c5AFaea627776889f9471042b972f80F84D53", DAPP);
-  console.log(upgraded.address);
-}
-async function upgraded_prd() {
-
-  // Upgrading
-  const DAPP = await ethers.getContractFactory("CTO");
   const upgraded = await upgrades.upgradeProxy("0xaDE76c8Bbc405c1fF367e10C786c81EeF0357454", DAPP);
   console.log(upgraded.address);
 }
+async function upgraded_CTOCatch_prd() {
 
-
-
-deployed_test()
+  // Upgrading
+  const DAPP = await ethers.getContractFactory("CTOCatch");
+  const upgraded = await upgrades.upgradeProxy("0x6Bf5709FE937612260A232C621902bc00225b1Fd", DAPP);
+  console.log(upgraded.address);
+}
